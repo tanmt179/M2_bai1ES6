caf613840972583d53dcf269836c00186012684a
tanmt179 <tantran179@gmail.com>
2022-07-05 08:02:35 +0700

    update

---
+++ demo.js
@@ -0,0 +1,17 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let square = (item) => item * item;
+// function arraySq(func, arr) {
+//     let newArr = [];
+//     arr.forEach((value) => {
+//         newArr.push(func(value));
+//     });
+//     return  newArr;
+// }
+
+// console.log(arraySq(square, arr));
+function sum(x, y, z) {
+    return x + y + z;
+}
+
+//const numbers = [1, 2, 3];
+console.log(sum(1,2,3));
